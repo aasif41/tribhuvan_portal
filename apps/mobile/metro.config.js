bc7bf9014da433e9010ddf93c6dbbe63f4328de5
aasif41 <mdaasif18092003@gmail.com>
2026-08-07 03:39:54 +0530

    fix(mobile): deduplicate monorepo react-native dependencies for SDK 54

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,14 +3,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-const defaultGetPolyfills = config.serializer?.getPolyfills || (() => []);
-
-config.serializer = {
-  ...config.serializer,
-  getPolyfills: (options) => [
-    require.resolve('./polyfills.js'),
-    ...defaultGetPolyfills(options),
-  ],
-};
-
 module.exports = config;
