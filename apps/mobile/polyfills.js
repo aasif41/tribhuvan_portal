fe55459f7dadbbca66e0154a6d6e369b1e043f69
aasif41 <mdaasif18092003@gmail.com>
2026-08-07 03:23:19 +0530

    fix(mobile): add PerformanceEntry and PerformanceObserver polyfills for Hermes runtime

--- apps/mobile/polyfills.js
+++ apps/mobile/polyfills.js
@@ -13,3 +13,38 @@ if (typeof globalThis.DOMException === 'undefined') {
     global.DOMException = DOMExceptionPolyfill;
   }
 }
+
+if (typeof globalThis.PerformanceEntry === 'undefined') {
+  function PerformanceEntryPolyfill(name, entryType, startTime, duration) {
+    this.name = name || '';
+    this.entryType = entryType || 'mark';
+    this.startTime = startTime || 0;
+    this.duration = duration || 0;
+  }
+  PerformanceEntryPolyfill.prototype.toJSON = function () {
+    return {
+      name: this.name,
+      entryType: this.entryType,
+      startTime: this.startTime,
+      duration: this.duration,
+    };
+  };
+
+  globalThis.PerformanceEntry = PerformanceEntryPolyfill;
+  if (typeof global !== 'undefined') {
+    global.PerformanceEntry = PerformanceEntryPolyfill;
+  }
+}
+
+if (typeof globalThis.PerformanceObserver === 'undefined') {
+  function PerformanceObserverPolyfill(callback) {
+    this.callback = callback;
+  }
+  PerformanceObserverPolyfill.prototype.observe = function () {};
+  PerformanceObserverPolyfill.prototype.disconnect = function () {};
+
+  globalThis.PerformanceObserver = PerformanceObserverPolyfill;
+  if (typeof global !== 'undefined') {
+    global.PerformanceObserver = PerformanceObserverPolyfill;
+  }
+}
